afb563060bedcab35dd7a1cbfb4b74383c42479f
Andressa Souza <amw.andressa@gmail.com>
2021-09-02 12:40:27 -0300

    Ajustando o conteúdo da tela de inicio

--- lib/widgets/elemento_grid_produtos.dart
+++ lib/widgets/elemento_grid_produtos.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lojinha_alura/modelos/movel.dart';
 import 'package:lojinha_alura/widgets/degrade_elemento_grid_produtos.dart';
 import 'package:lojinha_alura/widgets/imagem_elemento_grid_produtos.dart';
+import 'package:lojinha_alura/widgets/titulo_elemento_grid_produtos.dart';
 
 class ElementoGridProdutos extends StatelessWidget {
   final Movel movel;
@@ -11,11 +12,15 @@ class ElementoGridProdutos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: Alignment.center,
       children: [
         ImagemElementoGridProdutos(
           imagem: movel.foto,
         ),
         DegradeElementoGridProdutos(),
+        TituloElementoGridProdutos(
+          titulo: movel.titulo,
+        ),
       ],
     );
   }
