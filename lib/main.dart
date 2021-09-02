da830e2ffc20c75c7c1cf074d9ceecbe31881c4f
Andressa Souza <amw.andressa@gmail.com>
2021-09-02 12:57:00 -0300

    Ajustes na tela home com elementos Text e Divider

--- lib/main.dart
+++ lib/main.dart
@@ -26,56 +26,55 @@ class MyApp extends StatelessWidget {
 }
 
 class Inicio extends StatelessWidget {
-
   final List moveis = [
     {
       "titulo": "Mesa",
       "preco": 300,
       "foto": "movel1.jpeg",
       "descricao":
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
     },
     {
       "titulo": "Cadeira",
       "preco": 120,
       "foto": "movel2.jpg",
       "descricao":
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
     },
     {
       "titulo": "Manta",
       "preco": 200,
       "foto": "movel3.jpg",
       "descricao":
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
     },
     {
       "titulo": "Sofá Cinza",
       "preco": 800,
       "foto": "movel4.jpg",
       "descricao":
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
     },
     {
       "titulo": "Mesa de cabeceira",
       "preco": 400,
       "foto": "movel5.jpg",
       "descricao":
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
     },
     {
       "titulo": "Jogo de Cama",
       "preco": 250,
       "foto": "movel6.jpg",
       "descricao":
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
     },
     {
       "titulo": "Sofá Branco",
       "preco": 900,
       "foto": "movel7.jpg",
       "descricao":
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
     }
   ];
 
@@ -83,11 +82,34 @@ class Inicio extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      appBar: AppBarCustomizada(titulo: 'Lojinha Alura', ehPaginaCarrinho: false),
-      body: GridProdutos(
-        moveis: moveis,
+      appBar:
+          AppBarCustomizada(titulo: 'Lojinha Alura', ehPaginaCarrinho: false),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(left: 30.0, right: 20.0, top: 10.0),
+                  child: Divider(),
+                ),
+              ),
+              Text('Produtos'),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(left: 20.0, right: 30.0, top: 10.0),
+                  child: Divider(),
+                ),
+              ),
+            ],
+          ),
+          Flexible(
+            child: GridProdutos(
+              moveis: moveis,
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
